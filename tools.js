71a7f5f6e83d97e49a078fb91e3aa79f2059a26f
aditya gore <aditya_gore@hotmail.com>
2023-08-12 15:37:17 +0530

    first commit

---
+++ tools.js
@@ -0,0 +1,25 @@
+let optionsCont = document.querySelector(".options-cont");
+let optionsFlag = true;
+
+// true -> show tools, false -> hide tools
+optionsCont.addEventListener("click", (e) => {
+  optionsFlag = !optionsFlag;
+
+  if (optionsFlag) {
+    openTools();
+  } else {
+    closeTools();
+  }
+});
+
+function openTools() {
+  let iconElem = optionsCont.children[0];
+  iconElem.classList.remove("fa-times");
+  iconElem.classList.add("fa-bars");
+}
+
+function closeTools() {
+  let iconElem = optionsCont.children[0];
+  iconElem.classList.remove("fa-bars");
+  iconElem.classList.add("fa-times");
+}
